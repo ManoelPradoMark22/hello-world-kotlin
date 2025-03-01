db4b7bf360ed3e6a4c66f5bb6783ed3f2ac0216f
ManoelPradoMark22 <manoelprado.aecjr@gmail.com>
2025-03-01 20:10:43 -0300

    for, if and try catch

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+    println("Entre com o seu nome:")
+
+    val name = readln()
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Hello, $name!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("E qual a sua idade? (Por favor, insira um número inteiro positivo)")
+
+    try {
+        val age = readln().toInt()
+
+        if (age <= 0) {
+            println("Idade inválida! Insira um número positivo.")
+            return
+        }
+
+        for (i in 1..age) {
+            val ageText = if(i==age)  "A sua idade é $i!" else "$i..."
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            println(ageText)
+        }
+    }catch(e: Exception) {
+        println("Idade inválida! Por favor, insira um número inteiro positivo.")
     }
 }
